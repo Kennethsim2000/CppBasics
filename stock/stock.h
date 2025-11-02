c9f8c9a384a29e2d538f6bcaae210854296a795f
kenneth <kenneth12345645@gmail.com>
2025-11-02 09:51:23 +0800

    Wrote stock header file

---
+++ stock/stock.h
@@ -0,0 +1,32 @@
+#ifndef STOCK_H
+#define STOCK_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Stock
+{
+    string ticketSymbol_;
+    string exchange_;
+    string companyName_;
+    double marketCap_;
+
+public:
+    Stock();
+    Stock(string ticketSymbol, string exchange, string companyName, double marketCap);
+
+    void setTicketSymbol(string ticketSymbol);
+
+    void setExchange(string exchange);
+
+    void setCompanyName(string companyName);
+
+    void setMarketCap(double marketCap);
+
+    string &getTicketSymbol();
+
+    string &getExchange();
+};
+
+#endif
